b1a0353ca8832b8381b732f18d2a0ff534546e53
akinwale ayomide <akinwaleayomide55@gmail.com>
2021-12-21 02:49:00 +0100

    New files added

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 const express = require('express');
 
+const connectDb = require('./config/db');
+
 const app = express();
 
+//Connect Database
+connectDb();
+
+//Init Middleware
+app.use(express.json({extended: false}))
+
 app.get('/', (req, res) => res.send('API is running'))
+
+//Define routes
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+app.use('/api/profile', require('./routes/api/profile'))
+app.use('/api/post', require('./routes/api/post'))
 
 const PORT = process.env.PORT || 5000;
 
